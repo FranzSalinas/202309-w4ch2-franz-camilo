74e77ab1583faa84e82ff01758341b517c889dda
Franz <fernandofranquiz@gmail.com>
2023-10-31 18:00:25 +0100

    Add models of the form

---
+++ src/components/userforms/userforms.tsx
@@ -0,0 +1,17 @@
+import { User } from '../../models/user';
+
+export function UserForms() {
+  const intialState: User = {
+    userSurname: '',
+    birthdate: '',
+    gender: '',
+    email: '',
+    isOk: false,
+    password: '',
+    repeatPassword: '',
+    userName: '',
+    accountType: '',
+  };
+
+  return;
+}
